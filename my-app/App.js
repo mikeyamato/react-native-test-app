990134bc988fed5fc65028c38aa2a68877bea1ab
Mix Master Mike <mikeyamato@gmail.com>
2017-10-22 13:34:44 -0700

    adding initial files

---
+++ my-app/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Changes you make will automatically reload.</Text>
+        <Text>Shake your phone to open the developer menu.</Text>
+        <Text></Text>
+        <Text>Hello World!</Text>
+        <Text>This is fucking awesome!</Text>
+        <Text>What else can this thing do?</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+// export default class HelloWorldApp extends Component {
+//   render() {
+//     return (
+//       <Text>Hello World!</Text>
+//     );
+//   }
+// }
